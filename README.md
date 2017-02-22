f4c1c4429c62821e507043ab42f3f2cafa4c8453
Dmitri Kunin <whitemarten@gmail.com>
2017-02-23 00:33:15 +0300

    master:Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Pocket unread list
+
+> Simple service to get list of titles from your unread pocket list
+
+## Table of Contents
+
+- [Install](#install)
+- [Usage](#usage)
+- [Contribute](#contribute)
+- [License](#license)
+
+## Install
+
+Clone this repo, you have to have these variables ready to pass to docker: $POCKET_CONSUMER_KEY, $POCKET_ACCESS_TOKEN either put them in the env, they will be passed on to the docker run function, or modify docker run function in package.json
+To get the consumer key - create an application in [Pocker Developer console](https://getpocket.com/developer/), to get access_tocken you can use [Authentificator](http://reader.fxneumann.de/plugins/oneclickpocket/auth.php)
+
+```console
+    npm run docker-image-build
+    npm run docker-image-start
+```
+
+## Usage
+
+```console
+    curl http://localhost:5050/api/pocket 
+
+```
+
+```javascript
+    {
+        "count":13,
+        "list":[
+        {
+        "word_count":"1444",
+        "sort":0,
+        "title":"Building a JSON Tree View Component in Vue.js from Scratch in Six Steps",
+        "url":"https://devblog.digimondo.io/building-a-json-tree-view-component-in-vue-js-from-scratch-in-six-steps-ce0c05c2fdd8#.9r9dxy8cr"}]},//...
+```
+
+## Contribute
+
+PRs accepted.
+
+## License
+
+MIT © Dmitri Kunin
---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+const https = require('https');
+const express = require('express');
+const app = express();
+const { POCKET_CONSUMER_KEY, POCKET_ACCESS_TOKEN } = process.env;
+const port = 5050;
+
+function httpsRequest(options) {
+    return new Promise((resolve, reject) => {
+        https.get(options, response => {
+            let str = '';
+
+            response.on('data', function(chunk) {
+                str += chunk;
+            });
+
+            response.on('end', function() {
+                resolve(JSON.parse(str));
+            });
+        }).on('error', e => {
+            reject(e);
+        });
+    });
+}
+
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
+
+app.get('/api/pocket', function(req, res) {
+    httpsRequest({
+        host: 'getpocket.com',
+        path: `/v3/get?consumer_key=${POCKET_CONSUMER_KEY}&access_token=${POCKET_ACCESS_TOKEN}&count=100`
+    }).then(data => {
+        const keys = Object.keys(data.list);
+        const count = keys.length;
+        const newList = keys.reduce(function(newArray, singleItem) {
+            const { word_count, sort_id, given_title, given_url } = data.list[singleItem];
+            return newArray.concat({ word_count, sort: sort_id, title: given_title, url: given_url });
+        }, []).sort((a,b) => {
+            return a.sort - b.sort;
+        });
+        res.json({ count, list: newList });
+    });
+});
+
+app.listen(port);
