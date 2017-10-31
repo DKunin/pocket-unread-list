f3362afe8f27b3528faa26f2c0b6b3325361c6bf
Dmitri Kunin <whitemarten@gmail.com>
2017-10-31 07:43:08 +0300

    In progress

--- index.js
+++ index.js
@@ -4,22 +4,56 @@ const express = require('express');
 const app = express();
 const { POCKET_CONSUMER_KEY, POCKET_ACCESS_TOKEN } = process.env;
 const port = 5050;
+const querystring = require('querystring');
+var request = require('request');
 
 function httpsRequest(options) {
     return new Promise((resolve, reject) => {
-        https.get(options, response => {
-            let str = '';
+        https
+            .get(options, response => {
+                let str = '';
 
-            response.on('data', function(chunk) {
-                str += chunk;
-            });
+                response.on('data', function(chunk) {
+                    str += chunk;
+                });
 
-            response.on('end', function() {
-                resolve(JSON.parse(str));
+                response.on('end', function() {
+                    resolve(JSON.parse(str));
+                });
+            })
+            .on('error', e => {
+                reject(e);
             });
-        }).on('error', e => {
+    });
+}
+
+function httpPost(options, data) {
+    return new Promise(function(resolve, reject) {
+        const postData = JSON.stringify(data);
+        console.log(postData);
+        var req = https.request(
+            Object.assign(options, {
+                // headers: {
+                //     'Content-Type': 'application/x-www-form-urlencoded',
+                //     'Content-Length': postData.length
+                // }
+            }),
+            function(response) {
+                var str = '';
+                response.on('data', function(chunk) {
+                    str += chunk;
+                });
+
+                response.on('end', function() {
+                    resolve(str);
+                });
+            }
+        );
+        req.on('error', function(e) {
             reject(e);
         });
+        req.write(postData);
+        req.end();
     });
 }
 
@@ -40,27 +74,58 @@ app.get('/api/pocket', function(req, res) {
         const keys = Object.keys(data.list);
         const count = keys.length;
         const newList = keys
-            .reduce(
-                function(newArray, singleItem) {
-                    const {
-                        word_count,
-                        sort_id,
-                        given_title,
-                        given_url
-                    } = data.list[singleItem];
-                    return newArray.concat({
-                        word_count,
-                        sort: sort_id,
-                        title: given_title,
-                        url: given_url
-                    });
-                },
-                []
-            )
+            .reduce(function(newArray, singleItem) {
+                const {
+                    word_count,
+                    sort_id,
+                    given_title,
+                    given_url
+                } = data.list[singleItem];
+                return newArray.concat({
+                    word_count,
+                    sort: sort_id,
+                    title: given_title,
+                    url: given_url
+                });
+            }, [])
             .sort((a, b) => {
                 return a.sort - b.sort;
             });
         res.json({ count, list: newList });
+    });
+});
+
+app.get('/api/pocket/add', function(req, res) {
+    // httpPost(
+    //     {
+    //         host: '',
+    //         method: 'POST',
+    //         path: ''
+    //     },
+
+    // ).then(data => {
+    //     console.log(data);
+    //     res.json(data);
+    // });
+    var options = {
+        url:
+            'https://www.skepticink.com/humesapprentice/2017/10/30/stoic-philosophy-motivation',
+        time: Date.now(),
+        consumer_key: POCKET_CONSUMER_KEY,
+        access_token: POCKET_ACCESS_TOKEN
+    };
+    var url = 'https://getpocket.com/v3/add';
+    var opts = {
+        uri: url,
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'X-Accept': 'application/json'
+        },
+        body: JSON.stringify(options)
+    };
+
+    request.post(opts, function(error, response, body) {
+        res.send(body);
     });
 });
 
